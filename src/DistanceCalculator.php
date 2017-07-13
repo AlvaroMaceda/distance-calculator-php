cc65436ca02bd0af22e97c7fb25c545f41a940bc
Alvaro Maceda <alvaro@alvaromaceda.es>
2017-07-13 08:49:58 +0200

    Working...

--- src/DistanceCalculator.php
+++ src/DistanceCalculator.php
@@ -3,11 +3,16 @@ namespace DistanceCalculator;
 
 class DistanceCalculator
 {
+
+    const API_HOST = 'https://maps.googleapis.com';
+    const API_URL = '/maps/api/distancematrix/json';
+
     public function calculate($fileName)
     {
         $row = 1;
         if (($handle = fopen($fileName, "r")) !== false) {
             while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                $this->calculateDistance()
                 $num = count($data);
                 echo "<p> $num fields in line $row: <br /></p>\n";
                 $row++;
@@ -18,4 +23,16 @@ class DistanceCalculator
             fclose($handle);
         }
     }
+
+    private function calculateDistance($origin, $destination) {
+        $url = $this->generateRequestURL();
+        $xml = file_get_contents("http://www.example.com/file.xml");
+    }
+
+    private function generateRequestURL()
+    {
+        return DistanceCalculator::API_HOST . "/" . DistanceCalculator::API_URL;
+    }
+
+
 }
